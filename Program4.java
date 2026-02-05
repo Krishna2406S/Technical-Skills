1e3a28018b0f0f38701accdd6e292ebde5020b29
KRISHNA <kartikkrishnakumar18@gmail.com>
2026-02-05 11:49:26 +0530

    Program5.java

---
+++ Program4.java
@@ -0,0 +1,8 @@
+/* Ques.:
+Given an integer array A of size N,
+find and return the product array of the same size 
+where the ith element of the product array will be equal to the 
+product of all elements divided by the ith element of the array A.
+
+
+ */
